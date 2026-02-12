b1893bca93ac53533e331c2f705313fd829a781c
ElizabelyMiranda <bebelytounanet@hotmail.com>
2026-02-12 11:20:16 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -4,8 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
+int main() { 
+    printf(''Desafio Xadrez!\n");Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
